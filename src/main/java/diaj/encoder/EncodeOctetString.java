31c0e028f99d7f5e210e02e75fef118096ab106d
Vassil Kolarov <vas@vas.io>
2017-09-05 12:24:01 +0100

    first commit

--- src/main/java/diaj/encoder/EncodeOctetString.java
+++ src/main/java/diaj/encoder/EncodeOctetString.java
@@ -7,9 +7,6 @@ import java.nio.charset.Charset;
 public class EncodeOctetString implements EncodeOperation{
     @Override
     public byte[] encode(Object value) {
-        //return Bytes.concat(((String) value).getBytes(), new byte[]{0});
-        //return ((String) value).getBytes(Charset.forName());
         return Utils.hexStringToByteArray((String)value);
-        //return ((String)value).getBytes(Charset.defaultCharset());
     }
 }
